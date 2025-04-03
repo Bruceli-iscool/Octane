772607f0fe09937cb2f13f03e05fa4bbc100aa3a
Bruce Li <isanoob1897@outlook.com>
2025-04-02 21:03:18 -0700

    fix

--- src/test/java/dev/desktop/octane/AppTest.java
+++ src/test/java/dev/desktop/octane/AppTest.java
@@ -1,19 +1,2 @@
 package dev.desktop.octane;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import org.junit.jupiter.api.Test;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest {
-
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
-    }
-}
